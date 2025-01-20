8a765c4689dc8e49000d3cbc805b64e3c53bdc20
gonos <gonosanna@mail.ru>
2025-01-20 20:37:50 +0300

    feat: шаблон проекта

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,6 +9,10 @@ import { AppService } from './app.service';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'), // путь до директории с вашими статическими файлами
+      serveRoot: '/', // корень для сервирования файлов
     }),
   ],
   controllers: [AppController],
